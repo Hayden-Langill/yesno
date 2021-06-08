8bba8bf02e6e60afcb682d06a39287acd53c42a8
HaydenLangill <>
2021-06-08 14:38:41 +0000

    spaces

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ document.getElementById('button').addEventListener('click', maybe)
 function maybe () {
   number = document.getElementById('input').value
   number = parseInt(number)
-if (number >= 0) {
-  document.getElementById('guess').innerHTML = 'Your number is a postive!'
-} else {
-  document.getElementById('guess').innerHTML = 'Your number is a negative!'
-}
+  if (number >= 0) {
+    document.getElementById('guess').innerHTML = 'Your number is a postive!'
+  } else {
+    document.getElementById('guess').innerHTML = 'Your number is a negative!'
+  }
 }
