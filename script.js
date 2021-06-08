5c124570ca4cfaf22367190a0552c610f6a0a48e
HaydenLangill <>
2021-06-08 14:33:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let number = 0
+
+document.getElementById('button').addEventListener('click', maybe)
+
+function maybe () {
+  number = document.getElementById('input').value
+  number = parseInt(number)
+if (number >= 0) {
+  document.getElementById('guess').innerHTML = 'Your number is a postive!'
+} else {
+  document.getElementById('guess').innerHTML = 'Your number is a negative!'
+}
+}
